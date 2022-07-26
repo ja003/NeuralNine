7c7f3eae3fe511989b61224484d248fc8963b37f
Adam Jurik <adam.jurik@grip-digital.com>
2022-07-26 10:59:28 +0200

    Python Machine Learning Tutorial #2 - Linear Regression

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+from T_2_LinearRegression import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Python Machine Learning Tutorial #2 - Linear Regression
+t2_func()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
