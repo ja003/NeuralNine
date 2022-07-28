5f5a5f22cb6f8e6683ff5fde0f70379a7b55852c
Adam Jurík <ja004@seznam.cz>
2022-07-28 09:35:42 +0200

    Python Machine Learning Tutorial #3 - K-Nearest Neighbors Classification

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from MachineLearning.t2_LinearRegression.LinearRegression import *
+from MachineLearning.t3_KNearestNeighbors.yt_KNearestNeighbors import *
 
 # Python Machine Learning Tutorial #2 - Linear Regression
 # t2_func()
 
 # https://www.neuralnine.com/linear-regression-from-scratch-in-python/
-linear_regression_main()
+# linear_regression_main()
 
+# https://youtu.be/LwTIHEnnCAM
+k_nearest_main()
 
