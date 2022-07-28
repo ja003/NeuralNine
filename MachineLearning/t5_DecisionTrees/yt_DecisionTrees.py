a0b1f54aee892b34ed138244ad6c50ac4e893406
Adam Jurík <ja004@seznam.cz>
2022-07-28 10:48:49 +0200

    Python Machine Learning Tutorial #5 - Decision Trees and Random Forest Classification

---
+++ MachineLearning/t5_DecisionTrees/yt_DecisionTrees.py
@@ -0,0 +1,37 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+
+
+def decision_tree_main():
+    data = load_breast_cancer()
+
+    X = data.data
+    Y = data.target
+
+    # , random_state=23 => ensures same data and result every time
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
+
+    clf = SVC(kernel='linear', C=3)
+    clf.fit(x_train, y_train)
+
+    clf2 = KNeighborsClassifier(n_neighbors=3)
+    clf2.fit(x_train, y_train)
+
+    clf3 = DecisionTreeClassifier()
+    clf3.fit(x_train, y_train)
+
+    clf4 = RandomForestClassifier()
+    clf4.fit(x_train, y_train)
+
+    print(f'SVC: {clf.score(x_test, y_test)}')
+    print(f'KNN: {clf2.score(x_test, y_test)}')
+    print(f'DTC: {clf3.score(x_test, y_test)}')
+    print(f'RFC: {clf4.score(x_test, y_test)}')
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from MachineLearning.t2_LinearRegression.LinearRegression import *
 from MachineLearning.t3_KNearestNeighbors.yt_KNearestNeighbors import *
 from MachineLearning.t4_SupportVectorMachines.yt_SupportVectorMachines import *
+from MachineLearning.t5_DecisionTrees.yt_DecisionTrees import *
 
 # Python Machine Learning Tutorial #2 - Linear Regression
 # t2_func()
@@ -14,4 +15,8 @@ from MachineLearning.t4_SupportVectorMachines.yt_SupportVectorMachines import *
 
 # Python Machine Learning #4 - Support Vector Machines
 # https://youtu.be/99Eyw7Quacc
-support_vector_machines_main()
+# support_vector_machines_main()
+
+# Python Machine Learning Tutorial #5 - Decision Trees and Random Forest Classification
+# https://youtu.be/1RlrUoGe1Fg
+decision_tree_main()
