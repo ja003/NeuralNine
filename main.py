39bce307caaba1661a2f27839ec201936fb97ad5
Adam Jurík <ja004@seznam.cz>
2022-07-28 10:31:10 +0200

    Python Machine Learning #4 - Support Vector Machines

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from MachineLearning.t2_LinearRegression.LinearRegression import *
 from MachineLearning.t3_KNearestNeighbors.yt_KNearestNeighbors import *
+from MachineLearning.t4_SupportVectorMachines.yt_SupportVectorMachines import *
 
 # Python Machine Learning Tutorial #2 - Linear Regression
 # t2_func()
@@ -7,6 +8,10 @@ from MachineLearning.t3_KNearestNeighbors.yt_KNearestNeighbors import *
 # https://www.neuralnine.com/linear-regression-from-scratch-in-python/
 # linear_regression_main()
 
+# Python Machine Learning Tutorial #3 - K-Nearest Neighbors Classification
 # https://youtu.be/LwTIHEnnCAM
-k_nearest_main()
+# k_nearest_main()
 
+# Python Machine Learning #4 - Support Vector Machines
+# https://youtu.be/99Eyw7Quacc
+support_vector_machines_main()
